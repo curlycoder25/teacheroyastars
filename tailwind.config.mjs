501477cc69b662e1380b4699e402cac46f476554
curlycoder25 <zhala.azimlee@gmail.com>
2026-04-20 14:23:14 +0400

    pink

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				"primary": "#4885C1",
-				"secondary": "#D32F2F",
-				"tertiary": "#FFB300",
-				"surface": "#f3f6ff",
-				"on-surface": "#213041",
-				"on-surface-variant": "#4e5d6f",
-				"surface-container-high": "#d1e4ff",
-				"surface-container-lowest": "#ffffff",
-				"primary-fixed": "#83bdfd",
-				"tertiary-container": "#feb300",
-				// Digər rəngləri ehtiyac olduqca bura əlavə edə bilərsən
-			},
-			fontFamily: {
-				sans: ['Plus Jakarta Sans', 'sans-serif'],
-			},
-			borderRadius: {
-				'DEFAULT': '1rem',
-				'lg': '2rem',
-				'xl': '3rem',
-			}
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                "primary": "var(--primary)",
+                "secondary": "var(--secondary)",
+                "tertiary": "var(--tertiary)",
+                "surface": "var(--surface)",
+                "on-surface": "var(--on-surface)",
+                "on-surface-variant": "var(--on-surface-variant)",
+                "surface-container-high": "var(--surface-container-high)",
+                "surface-container-lowest": "var(--surface-container-lowest)",
+                "primary-fixed": "var(--primary-fixed)",
+                "tertiary-container": "var(--tertiary-container)",
+            },
+            fontFamily: {
+                sans: ['Plus Jakarta Sans', 'sans-serif'],
+            },
+            borderRadius: {
+                'DEFAULT': '1rem',
+                'lg': '2rem',
+                'xl': '3rem',
+            }
+        },
+    },
+    plugins: [],
 }
